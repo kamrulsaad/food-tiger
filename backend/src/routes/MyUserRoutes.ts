b68cc345787a75b6ae0069cdce11532d96baa653
kamrulsaad <skamrul2680@gmail.com>
2025-02-01 01:29:47 +0600

    Added Advanced security checks for auth in backend

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-// import { jwtCheck, jwtParse } from "../middleware/auth";
+import { jwtCheck } from "../middleware/auth";
 // import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // /api/my/user
 // router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
-router.post("/", MyUserController.createCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 // router.put(
 //   "/",
 //   jwtCheck,
