23e0cf3f4ff24e4af172226919128f5e1a189258
kamrulsaad <skamrul2680@gmail.com>
2025-02-01 00:48:35 +0600

    Set Up Authentication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
-// import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "sonner";
 
@@ -20,10 +20,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
-        {/* <Auth0ProviderWithNavigate> */}
+        <Auth0ProviderWithNavigate>
           <AppRoutes />
           <Toaster visibleToasts={1} position="top-right" richColors />
-        {/* </Auth0ProviderWithNavigate> */}
+        </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
   </React.StrictMode>
