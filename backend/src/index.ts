6048e6b912b0b9d7203fa11e276206bdab520915
kamrulsaad <skamrul2680@gmail.com>
2025-01-31 18:52:58 +0600

    Fresh frontend and backend install

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+// mongoose
+//   .connect(process.env.MONGODB_CONNECTION_STRING as string)
+//   .then(() => console.log("Connected to database!"));
+
+
+const app = express();
+
+app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
